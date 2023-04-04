b403ad88fb20567f6a8339ef3a6bb0c17cd9d54b
pangilinan_patrick <14168889-pangilinan_patrick@users.noreply.gitlab.com>
2023-04-04 16:33:03 +0800

    bound all relevant data to q-table

--- user-management-sys/src/pages/ListOfUsers.vue
+++ user-management-sys/src/pages/ListOfUsers.vue
@@ -22,6 +22,12 @@ export default {
     let rows = ref([]);
     let columns = ref([
       {
+        name: "id",
+        label: "ID",
+        align: "left",
+        field: "id",
+      },
+      {
         name: "name",
         label: "Full Name",
         align: "left",
@@ -39,19 +45,92 @@ export default {
         align: "left",
         field: "email",
       },
+      {
+        name: "street",
+        label: "Street",
+        align: "left",
+        field: "street",
+      },
+      {
+        name: "suite",
+        label: "Suite",
+        align: "left",
+        field: "suite",
+      },
+      {
+        name: "city",
+        label: "City",
+        align: "left",
+        field: "city",
+      },
+      {
+        name: "zipcode",
+        label: "Zipcode",
+        align: "left",
+        field: "zipcode",
+      },
+      {
+        name: "phone",
+        label: "Phone",
+        align: "left",
+        field: "phone",
+      },
+      {
+        name: "website",
+        label: "Website",
+        align: "left",
+        field: "website",
+      },
+      {
+        name: "companyName",
+        label: "Company Name",
+        align: "left",
+        field: "companyName",
+      },
+      {
+        name: "catchPhrase",
+        label: "Catch Phrase",
+        align: "left",
+        field: "catchPhrase",
+      },
+      {
+        name: "bs",
+        label: "BS",
+        align: "left",
+        field: "bs",
+      },
     ]);
+
+    // initialize mergedRows for nested data
+    const mergedRows = ref([]);
 
     const getUsers = () => {
       axios
         .get("https://jsonplaceholder.typicode.com/users")
         .then((response) => {
+          // impute the value of response into each row
           rows.value = response.data;
+          // iterate over each property of response.data
+          mergedRows.value = response.data.map((row) => {
+            return {
+              // add existing non-nested values from row
+              ...row,
+              // nested values
+              street: row.address.street,
+              suite: row.address.suite,
+              city: row.address.city,
+              zipcode: row.address.zipcode,
+              companyName: row.company.name,
+              catchPhrase: row.company.catchPhrase,
+              bs: row.company.bs,
+            };
+          });
         });
     };
 
     getUsers();
 
-    return { rows, columns };
+    return { rows, columns, mergedRows };
   },
 };
 </script>
@@ -59,6 +138,6 @@ export default {
 <template>
   <q-page class="q-ma-x1">
     <!-- table showing the data fetched from API -->
-    <q-table title="Todos" :rows="rows" :columns="columns" row-key="id" />
+    <q-table title="Todos" :rows="mergedRows" :columns="columns" row-key="id" />
   </q-page>
 </template>
